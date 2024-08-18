3d7b4a3e22b80e1272daa43f8e2d7d9d43f98260
qtng <qtngyt@gmail.com>
2024-08-18 22:20:34 +0200

    initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// let username = rl.question("What is your name? ", function(name) {
+//     console.log(`Hello ${name}!`);
+//     rl.close();
+// });
+
+// //rwarar
+// //test
+
+let equation = '(1+2)/2'
+let result = eval(equation)
+
+console.log(equation)
+console.log(result)
