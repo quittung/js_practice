47fc72f79cd621f50776dc6844664ee87840148b
HimikoStar <himikostar@yahoo.com>
2024-09-01 19:25:11 -0300

    Finding the Note corresponding to the Frequency
    Co-authored-by: quittung <quittung@users.noreply.github.com>

--- 03_chords/script.js
+++ 03_chords/script.js
@@ -5,8 +5,27 @@ let source;
 let bufferAnalyser;
 let emptyArray;
 let audioPlaying = false;
+let notes = [];
+let noteA4 = 440;
+let octaveFactor = 2;
+let stepFactor = octaveFactor ** (1 / 12);
 
+//! Creating the function for the action of clicking the button
 async function handleButtonClick() {
+  // Finding the pitch of C0
+  let noteA0 = noteA4 / octaveFactor ** 4;
+  let noteC0 = noteA0 / stepFactor ** 9;
+
+  // Make an array with all frequencies of the 0 octave
+
+  notes.push(noteC0);
+  for (let i = 0; i < 10; i++) {
+    notes.push(notes[notes.length - 1] * stepFactor);
+  }
+  console.log(notes);
+
+  // divide maxFreq between every note compare them to 2
+
   // Setup audio
   // await initAudio("audio/yougotmail.mp3");
   await initAudio("audio/sin_4.wav");
@@ -17,14 +36,15 @@ async function handleButtonClick() {
   // Start audio and draw the spectrum
   playAudio();
   drawSpectrum();
+  findFrequency();
 }
 
+//! Function that prints the information to the webpage
 function printInfo() {
   const infoDiv = document.getElementById("audioInfo");
 
   const sampleRate = audioContext.sampleRate;
   const fftSize = analyser.fftSize;
-  const binSize = sampleRate / analyser.fftSize;
 
   infoDiv.innerHTML = "";
   infoDiv.innerHTML += "Audio sample rate: " + sampleRate + " Hz <br/>";
@@ -36,6 +56,7 @@ function printInfo() {
     "Frequency bin width: " + sampleRate / fftSize + " Hz <br/>";
 }
 
+//! Initialize the Audio File
 async function initAudio(url) {
   // Get audio data
   const response = await fetch(url);
@@ -67,11 +88,13 @@ async function initAudio(url) {
   analyser.connect(audioContext.destination);
 }
 
+//! Play the audio File
 function playAudio() {
   source.start();
   audioPlaying = true;
 }
 
+//! Draw the spectrum
 function drawSpectrum() {
   // Canvas init
   const canvas = document.getElementById("fftCanvas");
@@ -84,18 +107,6 @@ function drawSpectrum() {
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     return;
   }
-
-  let freqArray = Array.from(frequencyData);
-  let maxIndex = freqArray.indexOf(Math.max(...frequencyData));
-  let maxFreq = (audioContext.sampleRate / analyser.fftSize) * (maxIndex + 0.5);
-
-  let frequencyDiv = document.getElementById("frequencyInfo");
-  frequencyDiv.innerHTML = "";
-  frequencyDiv.innerHTML += "Max value: " + Math.max(...freqArray) + "<br/>";
-  frequencyDiv.innerHTML += "Max index: " + maxIndex + "<br/>";
-  frequencyDiv.innerHTML +=
-    "Value at max index: " + freqArray[maxIndex] + "<br/>";
-  frequencyDiv.innerHTML += "Frequency played: " + maxFreq + "<br/>";
 
   // Make sure the analyser and frequency data are initialized
   if (!analyser || !frequencyData) {
@@ -133,6 +144,78 @@ function drawSpectrum() {
     // Move the drawing position to the right
     x += barWidth;
   }
+}
+
+//! Find the strongest frequency
+function findFrequency() {
+  // requesting updates for function findFrequency
+  if (audioPlaying) {
+    requestAnimationFrame(findFrequency);
+  }
+  // Make sure the analyser and frequency data are initialized
+  if (!analyser || !frequencyData) {
+    console.log("Analyser and frequency data are not initialized");
+
+    return;
+  }
+  // Finding the strongest bin and the frequency related to it
+  let freqArray = Array.from(frequencyData);
+  let maxIndex = freqArray.indexOf(Math.max(...frequencyData));
+  let maxFreq = (audioContext.sampleRate / analyser.fftSize) * (maxIndex + 0.5);
+
+  let frequencyDiv = document.getElementById("frequencyInfo");
+  frequencyDiv.innerHTML = "";
+  frequencyDiv.innerHTML += "Max value: " + Math.max(...freqArray) + "<br/>";
+  frequencyDiv.innerHTML += "Max index: " + maxIndex + "<br/>";
+  frequencyDiv.innerHTML +=
+    "Value at max index: " + freqArray[maxIndex] + "<br/>";
+  frequencyDiv.innerHTML += "Frequency played: " + maxFreq + "<br/>";
+
+  // Find the Pitch related to the Frequency
+  // divide the maxFreq between every note and see which one is closest to 2
+
+  // let frequency = maxFreq;
+  let frequency = maxFreq;
+  let octave = 0;
+
+  let freqMultiplier = [];
+  for (let i = 0; i < notes.length; i++) {
+    freqMultiplier.push(frequency / notes[i]);
+  }
+  //repeat the freqMultiplier and check
+  while (freqMultiplier[0] >= 2) {
+    octave++;
+    for (let i = 0; i < notes.length; i++) {
+      freqMultiplier[i] /= 2;
+    }
+  }
+
+  const goal = 1;
+
+  //TODO: replace by for loop
+  let closest = freqMultiplier.indexOf(
+    freqMultiplier.reduce((prev, curr) =>
+      Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev
+    )
+  );
+
+  let names = [
+    "C",
+    "C#/Db",
+    "D",
+    "D#/Eb",
+    "E",
+    "F",
+    "F#/Gb",
+    "G",
+    "G#/Ab",
+    "A",
+    "A#/Bb",
+    "B",
+  ];
+
+  frequencyDiv.innerHTML +=
+    "Note Played: " + names[closest] + " octave " + octave + "<br/>";
 }
 
 // Attach the initialization function to a button click
